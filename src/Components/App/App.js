ffc458d149617c7080b646f73a13a3cd7505603e
Renan Rodrigo <renanrb@ime.usp.br>
2019-04-26 21:21:11 -0300

    Build basic routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from '../Home/Home';
+import About from '../About/About';
+import Subscribe from '../Subscribe/Subscribe';
 
-class App extends React.Component{
-  render(){
-    return null;
+class App extends React.Component {
+  render() {
+    return (<Router>
+      <Route path="/" exact component={Home}/>
+      <Route path="/about" exact component={About}/>
+      <Route path="/subscribe" exact component={Subscribe}/>
+    </Router>);
   }
 }
 
---
+++ src/Components/Subscribe/Subscribe.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Subscribe extends React.Component {
+  render() {
+    return <div>INSCREVA-SE NA NEWSLETTER</div>;
+  }
+}
+
+export default Subscribe;
